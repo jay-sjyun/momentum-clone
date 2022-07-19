aaa8bbe4dcce746a0b27408afee80280dea7151e
sjyun-careyou <sjyun@careyou.org>
2022-07-20 05:08:07 +0900

    레이아웃 및 스타일 1차

--- js/background.js
+++ js/background.js
@@ -2,8 +2,9 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url("../img/${chosenImage}")`;
+// 이미지 교체 필요(해상도 및 파일 크기 초과)
--- js/greetings.js
+++ js/greetings.js
@@ -5,26 +5,26 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    // greeting.innerText = "Hello " + username;
-    paintGreetings(username);
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  // greeting.innerText = "Hello " + username;
+  paintGreetings(username);
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello, ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername === null) {
-    //show the form
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
-    loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    //show the greetings
-    paintGreetings(savedUsername);
+  //show the greetings
+  paintGreetings(savedUsername);
 }

c4076fb3747c62beabb1c49ae3501c73301695d8
sjyun-careyou <sjyun@careyou.org>
2022-11-10 02:05:52 +0900

    배경이미지 교체

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
@@ -7,4 +7,3 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 // bgImage.src = `img/${chosenImage}`;
 
 document.body.style.backgroundImage = `url("../img/${chosenImage}")`;
-// 이미지 교체 필요(해상도 및 파일 크기 초과)

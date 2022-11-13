5ac902e28c781e5f936962565cd8bc8494687ff3
sjyun-careyou <sjyun@careyou.org>
2022-11-14 06:27:34 +0900

    날씨 기능 추가 및 반응형 구현

--- js/main.js
+++ js/main.js
@@ -35,6 +35,74 @@ const httpRequest = {
   },
 };
 
+function weatherApp() {
+  const $locationInfo = document.querySelector(".js-weather span:first-of-type");
+  const $weatherInfo = document.querySelector(".js-weather span:nth-child(2)");
+  const $weatherIcon = document.querySelector(".js-weather-icon");
+
+  const API_KEY = "a155f00c11c73a1d9b10cc6ab623767b";
+
+  async function fetchWeatherInfo(lat, lon) {
+    const response = await httpRequest.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const dataObj = await response.json();
+    const currentWeather = dataObj.weather[0].main;
+    let weatherIcon;
+
+    switch (currentWeather) {
+      case "Clear":
+        weatherIcon = "Sunny";
+        break;
+      case "Clouds":
+        weatherIcon = "Cloud";
+        break;
+      case "Thunderstorm":
+        weatherIcon = "Thunderstorm";
+        break;
+      case "Drizzle":
+        weatherIcon = "Rainy";
+        break;
+      case "Rain":
+        weatherIcon = "Rainy";
+        break;
+      case "Snow":
+        weatherIcon = "Weather Snowy";
+        break;
+      case "Mist":
+      case "Smoke":
+      case "Haze":
+      case "Dust":
+      case "Fog":
+      case "Sand":
+      case "Ash":
+      case "Squall":
+        weatherIcon = "Foggy";
+        break;
+      case "Tornado":
+        weatherIcon = "Cyclone";
+        break;
+      default:
+        weatherIcon = "";
+    }
+
+    $locationInfo.textContent = `${dataObj.name}, `;
+    $weatherInfo.textContent = currentWeather;
+    $weatherIcon.textContent = weatherIcon;
+  }
+
+  function geoSuccess(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    fetchWeatherInfo(lat, lon).catch(console.error);
+  }
+
+  function geoError() {
+    alert("Failed to get your location :(");
+  }
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+}
+
 function signInAndOut() {
   const $signArea = document.querySelector(".js-sign-wrap");
   const $signForm = $signArea.querySelector(".js-sign-form");
@@ -71,7 +139,7 @@ function signInAndOut() {
     e.preventDefault();
     const username = $usernameInput.value;
 
-    httpRequest.post(URL, { id: username }).catch((e) => console.error(e));
+    httpRequest.post(URL, { id: username }).catch(console.error);
 
     currentUsername = username;
     localStorage.setItem(USERNAME_KEY, username);
@@ -102,7 +170,7 @@ function toDoApp() {
   let todos = [];
 
   function saveToDo() {
-    httpRequest.put(`${URL}/${currentUsername}`, { list: todos }).catch((e) => console.error(e));
+    httpRequest.put(`${URL}/${currentUsername}`, { list: todos }).catch(console.error);
   }
 
   function deleteToDo(e) {
@@ -139,15 +207,73 @@ function toDoApp() {
 
   $toDoForm.addEventListener("submit", addToDo);
 
-  httpRequest
-    .get(`${URL}/${currentUsername}`)
-    .then((response) => response.json())
-    .then((data) => {
-      data.list.forEach((todo) => {
-        paintToDo(todo);
-        todos.push(todo);
-      });
-      saveToDo();
-    })
-    .catch((e) => console.error(e));
+  async function fetchToDo() {
+    const response = await httpRequest.get(`${URL}/${currentUsername}`);
+    const data = await response.json();
+
+    data.list.forEach((todo) => {
+      paintToDo(todo);
+      todos.push(todo);
+    });
+    saveToDo();
+  }
+
+  fetchToDo().catch(console.error);
+}
+
+function ctrlBg() {
+  const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+  document.body.style.backgroundImage = `url("../img/${chosenImage}")`;
+}
+
+function clockApp() {
+  const $clock = document.querySelector(".js-clock-time");
+  const $midday = document.querySelector(".js-clock-midday");
+
+  function getClock() {
+    const date = new Date();
+    // let hours = String(date.getHours()).padStart(2, "0");
+    let hours = date.getHours();
+    let midday;
+    if (hours > 12) {
+      hours = String(hours - 12).padStart(2, "0");
+      midday = "PM";
+    } else {
+      hours = String(hours).padStart(2, "0");
+      midday = "AM";
+    }
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    $clock.textContent = `${hours}:${minutes}:${seconds}`;
+    $midday.textContent = midday;
+  }
+
+  getClock();
+  setInterval(getClock, 1000);
+}
+
+function quotesApp() {
+  const quotes = [
+    { quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde" },
+    { quote: "You only live once, but if you do it right, once is enough.", author: "Mae West" },
+    { quote: "Be the change that you wish to see in the world.", author: "Mahatma Gandhi" },
+    { quote: "In three words I can sum up everything I've learned about life: it goes on.", author: "Robert Frost" },
+    { quote: "If you tell the truth, you don't have to remember anything.", author: "Mark Twain" },
+    { quote: "I have not failed. I've just found 10,000 ways that won't work.", author: "Thomas A. Edison" },
+    { quote: "It is never too late to be what you might have been.", author: "George Eliot" },
+    { quote: "Life isn't about finding yourself. Life is about creating yourself.", author: "George Bernard Shaw" },
+    { quote: "Life is like riding a bicycle. To keep your balance, you must keep moving.", author: "Albert Einstein" },
+    { quote: "It takes courage to grow up and become who you really are.", author: "E.E. Cummings" },
+  ];
+
+  const $quote = document.querySelector(".js-quote q:first-child");
+  const $author = document.querySelector(".js-quote span:last-child");
+
+  const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  $quote.textContent = `"${todaysQuote.quote}"`;
+  $author.textContent = todaysQuote.author;
 }

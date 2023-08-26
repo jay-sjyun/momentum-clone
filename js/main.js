8bc8ebd0763d40c927c766434c4fc3e277c045fc
jay.sjyun <jay.sjyun@gmail.com>
2023-08-26 14:52:06 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,188 +1,124 @@
 "use strict";
 
-const URL = "https://my-json-server.typicode.com/dev-yun0525/fakedb/todos";
-const USERNAME_KEY = "username";
-
+const CURRENTUSER_KEY = "currentUser";
+const HIDDEN_CLASSNAME = "hidden";
 // 로컬스토리지에 저장된 사용자 정보 불러오기
-let currentUsername = localStorage.getItem(USERNAME_KEY);
+let currentUser = localStorage.getItem(CURRENTUSER_KEY);
 
-// AJAX
-const httpRequest = {
-  get(url) {
-    return fetch(url);
-  },
-  post(url, payload) {
-    return fetch(url, {
-      method: "POST",
-      headers: { "content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    });
-  },
-  put(url, payload) {
-    return fetch(url, {
-      method: "PUT",
-      headers: { "content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    });
-  },
-  patch(url, payload) {
-    return fetch(url, {
-      method: "PATCH",
-      headers: { "content-Type": "application/json" },
-      body: JSON.stringify(payload),
-    });
-  },
-  delete(url) {
-    return fetch(url, { method: "DELETE" });
-  },
-};
-
-// 모달 조작
-const ctrlModal = () => {
-  const $modals = document.querySelectorAll(".js-modal");
-
-  const openModal = (e) => {
-    if (!e.target.classList.contains("js-nav__btn")) return;
-    const modalCategory = e.target.dataset.modalCategory;
-    const $targetModal = document.querySelector(`.js-modal[data-modal-category='${modalCategory}']`);
-
-    $modals.forEach((elm) => elm.classList.remove("on"));
-    $targetModal.showModal();
-    $targetModal.classList.add("on");
-  };
-
-  const closeModal = (e) => {
-    if (!e.target.parentElement.classList.contains("js-modal__close-btn")) return;
-
-    $modals.forEach((elm) => {
-      if (elm.contains(e.target)) {
-        elm.classList.remove("on");
-        elm.close();
-      }
-    });
-  };
-
-  document.body.addEventListener("click", openModal);
-  document.body.addEventListener("click", closeModal);
-};
-
-// 로그인 및 로그아웃
-function signInAndOut() {
-  const $signArea = document.querySelector(".js-sign-wrap");
-  const $signForm = $signArea.querySelector(".js-sign-form");
-  const $signInQuestion = $signArea.querySelector(".js-question");
-  const $usernameInput = $signArea.querySelector(".js-username");
-  const $signInBtn = $signArea.querySelector(".js-btn-signin");
-  const $greetingTxt = $signArea.querySelector(".js-greeting");
-  const $signOutBtn = $signArea.querySelector(".js-btn-signout");
-  const $toDo = document.querySelector(".js-todo-wrap");
-
-  const HIDDEN_CLASSNAME = "hidden";
-  const ON_CLASSNAME = "on";
-
-  /** 로그인 UI 숨기기 */
-  function hideSignInUI() {
-    $signInQuestion.classList.add(HIDDEN_CLASSNAME);
-    $signInBtn.classList.add(HIDDEN_CLASSNAME);
-  }
-
-  /** 할일 보이기 */
-  function showToDo() {
-    $toDo.classList.remove(HIDDEN_CLASSNAME);
-    setTimeout(() => {
-      $toDo.classList.add(ON_CLASSNAME);
-    }, 400);
-  }
-
-  /** 환영인사 보이기 */
-  function paintGreeting(username) {
-    $greetingTxt.textContent = `Hi, ${username}`;
-    $greetingTxt.classList.remove(HIDDEN_CLASSNAME);
-    setTimeout(() => {
-      $greetingTxt.classList.add(ON_CLASSNAME);
-    }, 10);
-    $signOutBtn.classList.remove(HIDDEN_CLASSNAME);
-    setTimeout(() => {
-      $signOutBtn.classList.add(ON_CLASSNAME);
-    }, 200);
-  }
-
-  /** 로그아웃 처리 */
-  function handleSignOut() {
-    localStorage.removeItem(USERNAME_KEY);
-    location.reload();
-  }
-
-  /** 로그인 처리 */
-  function handleSignIn(e) {
-    e.preventDefault();
-    const username = $usernameInput.value;
-
-    // DB에 사용자 저장
-    httpRequest.post(URL, { id: username }).catch(console.error);
-
-    currentUsername = username;
-    localStorage.setItem(USERNAME_KEY, username);
-    hideSignInUI();
-    $signOutBtn.addEventListener("click", handleSignOut);
-    paintGreeting(username);
-    showToDo();
-    toDoApp();
-  }
-
-  $signForm.addEventListener("submit", handleSignIn);
-
-  // 사용자가 로컬스토리지에 저장되어 있는 경우
-  if (currentUsername !== null) {
-    hideSignInUI();
-    $signOutBtn.addEventListener("click", handleSignOut);
-    paintGreeting(currentUsername);
-    showToDo();
-    toDoApp();
+// 환영 인사
+const showGreeting = (username) => {
+  const $greeting = document.querySelector(".js-greeting");
+  const date = new Date();
+  const hour = date.getHours();
+  let greetingTxt;
+  if (hour > 4 && hour < 12) {
+    greetingTxt = "Good morning";
+  } else if (hour > 12 && hour < 20) {
+    greetingTxt = "Good afternoon";
   } else {
-    // 로컬스토리지에 사용자가 없을 경우
-    $signInQuestion.classList.remove(HIDDEN_CLASSNAME);
-    setTimeout(() => {
-      $signInQuestion.classList.add(ON_CLASSNAME);
-    }, 10);
-    $signInBtn.classList.remove(HIDDEN_CLASSNAME);
-    setTimeout(() => {
-      $signInBtn.classList.add(ON_CLASSNAME);
-    }, 200);
+    greetingTxt = "Good evening";
   }
-}
+  $greeting.textContent = `${greetingTxt}, ${username}`;
+};
+
+// 할일 앱
+const toDoApp = () => {
+  const $toDoForm = document.querySelector(".js-todo-form");
+  const $toDoInput = document.querySelector(".js-input--todo");
+  const $toDoList = document.querySelector(".js-todo-list");
+
+  // 할일 객체를 배열에 저장
+  let todos = [];
+
+  // 할일 배열 저장
+  const saveToDo = () => {
+    localStorage.setItem(currentUser, JSON.stringify(todos));
+  };
+
+  // 할일 객체 삭제
+  const deleteToDo = (e) => {
+    todos = todos.filter((toDoObj) => +toDoObj.id !== +e.target.parentNode.id);
+    $toDoList.removeChild(e.target.parentNode);
+    saveToDo();
+  };
+
+  // 할일 html 요소 생성
+  const paintToDo = (toDoObj) => {
+    const $li = document.createElement("li");
+    $li.id = toDoObj.id;
+    //텍스트 추가
+    const $span = document.createElement("span");
+    $span.textContent = toDoObj.content;
+    $span.classList.add("todo-content");
+    $toDoInput.value = "";
+    $li.appendChild($span);
+    //삭제 버튼 추가
+    const $delBtn = document.createElement("button");
+    $delBtn.textContent = "DELETE";
+    $delBtn.classList.add("btn--del-todo");
+    $li.appendChild($delBtn);
+    $delBtn.addEventListener("click", deleteToDo);
+    $toDoList.appendChild($li);
+  };
+
+  // 할일 객체 추가
+  const addToDo = (e) => {
+    e.preventDefault();
+    const toDoObj = {};
+    toDoObj.id = Date.now();
+    toDoObj.content = $toDoInput.value;
+    paintToDo(toDoObj);
+    todos.push(toDoObj);
+    saveToDo();
+  };
+
+  $toDoForm.addEventListener("submit", addToDo);
+
+  // 로컬 스토리지에서 현재 사용자 이름으로 저장된 할일 불러오기
+  if (localStorage.getItem(currentUser) === null) {
+    return;
+  } else {
+    todos = JSON.parse(localStorage.getItem(currentUser));
+    todos.forEach((todo) => {
+      paintToDo(todo);
+    });
+  }
+};
 
 // 뉴스 앱
 const newsApp = () => {
-  const $newsOpenBtn = document.querySelector(".js-nav__btn[data-modal-category='news']");
-  const $newsModal = document.querySelector(".js-modal[data-modal-category='news']");
-  const $loadingTxt = $newsModal.querySelector(".js-loading-text");
-  const $newsList = $newsModal.querySelector(".js-news-list");
+  const $loadingTxt = document.querySelector(".js-loading-text");
+  const $newsList = document.querySelector(".js-news-list");
   const $newsFragment = document.createDocumentFragment();
 
-  const NEWS_API = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=509e5683a97a4d0995f41662e21d9520`;
+  const NEWS_API = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=956d67a3a5fe45a2b60bb2f789870a46`;
 
   const paintArticle = (data) => {
     const $li = document.createElement("li");
     $li.classList.add("news-item");
+    const fullHeadline = data.title;
+    // 뉴스 헤드라인 추출
+    const headline = fullHeadline.substring(0, fullHeadline.indexOf("-") - 1);
     const $title = document.createElement("a");
-    $title.textContent = `${data.title}`;
+    $title.textContent = `${headline}`;
     $title.href = `${data.url}`;
     $title.target = `_blank`;
     $title.classList.add("news-title");
     $li.append($title);
-    const $author = document.createElement("span");
-    $author.textContent = `${data.author}`;
-    $author.classList.add("news-author");
-    $li.append($author);
+    // 뉴스 출처 추출
+    const source = fullHeadline.substring(fullHeadline.lastIndexOf("-") + 1);
+    const $source = document.createElement("span");
+    $source.textContent = `${source}`;
+    $source.classList.add("news-source");
+    $li.append($source);
     $newsFragment.append($li);
   };
 
   const fetchNews = async () => {
-    $loadingTxt.textContent = "불러오는 중...";
-    const response = await httpRequest.get(NEWS_API);
+    $loadingTxt.textContent = "Loading...";
+    const response = await fetch(NEWS_API);
     const data = await response.json();
-    const articles = data.articles;
+    const articles = data.articles.slice(0, 10);
     if (articles) {
       $loadingTxt.textContent = "";
       articles.forEach((article) => paintArticle(article));
@@ -190,96 +126,27 @@ const newsApp = () => {
     }
   };
 
-  $newsOpenBtn.addEventListener("click", () => {
-    $newsList.textContent = "";
-    fetchNews().catch((reason) => {
-      console.error(reason);
-      $loadingTxt.textContent = "불러오기 실패";
-    });
+  $newsList.textContent = "";
+  fetchNews().catch((reason) => {
+    console.error(reason);
+    $loadingTxt.textContent = "Loading failed.";
   });
 };
 
-// 할일 앱
-function toDoApp() {
-  const $toDo = document.querySelector(".js-todo-wrap");
-  const $toDoForm = $toDo.querySelector(".js-todo-form");
-  const $newToDoInput = $toDoForm.querySelector(".js-new-todo");
-  const $toDoList = $toDo.querySelector(".js-todo-list");
-
-  // 할일 객체를 배열에 저장
-  let todos = [];
-
-  /** 할일 배열 저장 */
-  function saveToDo() {
-    httpRequest.put(`${URL}/${currentUsername}`, { list: todos }).catch(console.error);
-  }
-
-  /** 할일 객체 삭제 */
-  function deleteToDo(e) {
-    todos = todos.filter((toDoObj) => +toDoObj.id !== +e.target.parentNode.id);
-    $toDoList.removeChild(e.target.parentNode);
-    saveToDo();
-  }
-
-  /** 할일 객체 그리기 */
-  function paintToDo(toDoObj) {
-    const $li = document.createElement("li");
-    $li.id = toDoObj.id;
-    //텍스트 추가
-    const $span = document.createElement("span");
-    $span.textContent = toDoObj.content;
-    $newToDoInput.value = "";
-    $li.appendChild($span);
-    //삭제 버튼 추가
-    const delBtn = document.createElement("button");
-    delBtn.textContent = "DELETE";
-    $li.appendChild(delBtn);
-    delBtn.addEventListener("click", deleteToDo);
-    $toDoList.appendChild($li);
-  }
-
-  /** 할일 객체 추가 */
-  function addToDo(e) {
-    e.preventDefault();
-    const toDoObj = {};
-    toDoObj.id = Date.now();
-    toDoObj.content = $newToDoInput.value;
-    paintToDo(toDoObj);
-    todos.push(toDoObj);
-    saveToDo();
-  }
-
-  $toDoForm.addEventListener("submit", addToDo);
-
-  /** DB에서 할일 데이터 불러오기 */
-  async function fetchToDo() {
-    // DB에서 사용자 이름 기반으로 데이터 검색
-    const response = await httpRequest.get(`${URL}/${currentUsername}`);
-    const data = await response.json();
-
-    // 할일 데이터 불러오기 및 후속 처리
-    data.list.forEach((todo) => {
-      paintToDo(todo);
-      todos.push(todo);
-    });
-    saveToDo();
-  }
-
-  fetchToDo().catch(console.error);
-}
-
 // 날씨 앱
 const weatherApp = () => {
-  const $locationInfo = document.querySelector(".js-weather span:first-of-type");
-  const $weatherInfo = document.querySelector(".js-weather span:nth-child(2)");
+  const $location = document.querySelector(".js-location");
+  const $temper = document.querySelector(".js-temper");
   const $weatherIcon = document.querySelector(".js-weather-icon");
 
   const API_KEY = "a155f00c11c73a1d9b10cc6ab623767b";
 
-  /** 위치정보를 받아 날씨정보 불러오기(AJAX) 및 데이터 후속 처리 */
+  // 위치정보를 받아 날씨정보 불러오기(AJAX) 및 데이터 후속 처리
   const fetchWeatherInfo = async (lat, lon) => {
-    const response = await httpRequest.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`);
     const dataObj = await response.json();
+    $location.textContent = `${dataObj.name},`;
+    $temper.textContent = `${Math.round(dataObj.main.temp)}°C`;
     const currentWeather = dataObj.weather[0].main;
     let weatherIcon;
 
@@ -319,65 +186,65 @@ const weatherApp = () => {
         weatherIcon = "";
     }
 
-    $locationInfo.textContent = `${dataObj.name}, `;
-    $weatherInfo.textContent = currentWeather;
     $weatherIcon.textContent = weatherIcon;
   };
 
-  /** 위치정보 취득 성공 처리 */
-  function geoSuccess(position) {
+  // 위치정보 취득 성공 처리
+  const geoSuccess = (position) => {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
 
-    /** 취득한 위치정보 바탕으로 날씨정보 불러오기 (AJAX) */
+    // 취득한 위치정보 바탕으로 날씨정보 불러오기 (AJAX)
     fetchWeatherInfo(lat, lon).catch(console.error);
-  }
+  };
 
-  /** 위치정보 취득 실패 처리 */
-  function geoError() {
+  // 위치정보 취득 실패 처리
+  const geoError = () => {
     alert("Failed to get your location :(");
-  }
+  };
 
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
 };
 
+// 시계 앱
+const clockApp = () => {
+  const $time = document.querySelector(".js-time");
+  const $midday = document.querySelector(".js-midday");
+  let hour;
+
+  const getTime = () => {
+    const date = new Date();
+    hour = date.getHours();
+    let midday;
+    if (hour > 12) {
+      hour = String(hour - 12).padStart(2, "0");
+      midday = "PM";
+    } else {
+      hour = String(hour).padStart(2, "0");
+      midday = "AM";
+    }
+    const minute = String(date.getMinutes()).padStart(2, "0");
+
+    $time.textContent = `${hour}:${minute}`;
+    $midday.textContent = midday;
+  };
+
+  getTime();
+  setInterval(getTime, 1000);
+};
+
 // 배경 설정
-function ctrlBg() {
+const randomBg = () => {
   const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
   const chosenImage = images[Math.floor(Math.random() * images.length)];
 
   document.body.style.backgroundImage = `url("img/${chosenImage}")`;
-}
-
-// 시계 앱
-function clockApp() {
-  const $clock = document.querySelector(".js-clock-time");
-  const $midday = document.querySelector(".js-clock-midday");
-
-  function getClock() {
-    const date = new Date();
-    let hours = date.getHours();
-    let midday;
-    if (hours > 12) {
-      hours = String(hours - 12).padStart(2, "0");
-      midday = "PM";
-    } else {
-      hours = String(hours).padStart(2, "0");
-      midday = "AM";
-    }
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    // const seconds = String(date.getSeconds()).padStart(2, "0");
-    $clock.textContent = `${hours}:${minutes}`;
-    $midday.textContent = midday;
-  }
-
-  getClock();
-  setInterval(getClock, 1000);
-}
+};
+randomBg();
 
 // 인용구 앱
-function quotesApp() {
+const quotesApp = () => {
   const quotes = [
     { quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde" },
     { quote: "You only live once, but if you do it right, once is enough.", author: "Mae West" },
@@ -398,4 +265,89 @@ function quotesApp() {
 
   $quote.textContent = `"${todaysQuote.quote}"`;
   $author.textContent = todaysQuote.author;
+};
+
+// 모달 조작
+const ctrlModal = () => {
+  const $todoBtn = document.querySelector(".js-show-todo");
+  const $newsBtn = document.querySelector(".js-show-news");
+  const $todoModal = document.querySelector(".js-todo-modal");
+  const $newsModal = document.querySelector(".js-news-modal");
+
+  toDoApp();
+  $todoBtn.addEventListener("click", () => {
+    $todoModal.showModal();
+  });
+  $newsBtn.addEventListener("click", () => {
+    $newsModal.showModal();
+    newsApp();
+  });
+};
+
+// UI 보이기
+const showUi = (element) => {
+  element.classList.remove(HIDDEN_CLASSNAME);
+};
+
+// 로그인 UI 숨기기
+const hideSignInForm = () => {
+  const $signInForm = document.querySelector(".js-signin-form");
+  $signInForm.classList.add(HIDDEN_CLASSNAME);
+};
+
+// 로그아웃
+const signOutProcess = () => {
+  const $signOutBtn = document.querySelector(".js-btn--signout");
+
+  $signOutBtn.addEventListener("click", () => {
+    localStorage.removeItem(CURRENTUSER_KEY);
+    location.reload();
+  });
+};
+
+// 홈 UI 초기화
+const initHomeUi = (username) => {
+  const $header = document.querySelector(".js-header");
+  const $home = document.querySelector(".js-home-wrap");
+  const $footer = document.querySelector(".js-footer");
+
+  hideSignInForm();
+  showUi($header);
+  ctrlModal();
+  weatherApp();
+  showUi($home);
+  clockApp();
+  showGreeting(username);
+  showUi($footer);
+  quotesApp();
+  signOutProcess();
+};
+
+// 로그인
+const signInProcess = () => {
+  const $signInForm = document.querySelector(".js-signin-form");
+  const $usernameInput = document.querySelector(".js-input--username");
+
+  // 로그인 핸들러
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const username = $usernameInput.value;
+    currentUser = username;
+    localStorage.setItem(CURRENTUSER_KEY, username);
+    initHomeUi(currentUser);
+  };
+
+  $signInForm.addEventListener("submit", handleSignIn);
+};
+
+// 사용자가 로컬스토리지에 저장되어 있는 경우
+if (currentUser !== null) {
+  initHomeUi(currentUser);
+} else {
+  // 로컬스토리지에 사용자가 없을 경우
+  showUi(document.querySelector(".js-signin-form"));
+  signInProcess();
+  // setTimeout(() => {
+  //   $signInQuestion.classList.add(ON_CLASSNAME);
+  // }, 10);
 }
